21f0ef48cb6327f96d3dfb3052b490a771ca69bc
ityeri <ityeri9@gmail.com>
2026-08-28 23:54:20 +0900

    refactor: All config files have been replaced to .lua script file

---
+++ monitors.lua
@@ -0,0 +1,22 @@
+hl.monitor({
+  output   = "HDMI-A-1",
+  mode     = "1920x1080@100.0",
+  position = "840x312",
+  scale    = 1.0
+})
+hl.monitor({
+  output    = "HDMI-A-1",
+  transform = 1 -- 90 degrees (rotated)
+})
+hl.monitor({
+  output   = "DP-1",
+  mode     = "2560x1080@144.0",
+  position = "1920x426",
+  scale    = 1.0
+})
+hl.monitor({
+  output   = "DP-2",
+  mode     = "2560x1440@60.0",
+  position = "1920x1506",
+  scale    = 2.13
+})
